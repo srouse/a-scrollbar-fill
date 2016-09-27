76ce29111ef78a0dc996d0c5423a46288e458a7f
Scott Rouse <scott.rouse@gmail.com>
2016-09-27 08:34:11 -0500

    first commit. All the initial working code committed.

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # a-scrollbar-fill
-Javascript shim to compensate for scrollbar percentage offsets.
+Javascript shim to create scrolling divs with content that ignores scrollbar
+width.
+
+It figures out the width of the browser's scrollbar and styles the scrollbar to blend
+into the background. (sans Edge and Firefox)
+
+There are two usages:
+- a-scrollbar-fill
+This forces the scrollbar to show for every browser. Edge/Firefox doesn't allow
+styling so it will show default scrollbar.
+
+- a-scrollbar-fill-auto
+Same except that it allows the scrollbar to show/hide. This will act the same
+as overflow-y: auto for Edge/Firefox. It's good for simple situations where it
+matters if showing the scrollbar all the time doesn't work but you still
+want the dominate browsers to look good.
---
+++ a-scrollbar-fill.js
@@ -0,0 +1,104 @@
+
+
+function createScrollbarFill () {
+
+    var scrollbar_style_str = '\
+                    .a-scrollbar-fill::-webkit-scrollbar,\
+                    .a-scrollbar-fill-auto::-webkit-scrollbar {\
+                        width: 10px !important;\
+                        height: 10px !important;\
+                    }\
+                    .a-scrollbar-fill::-webkit-scrollbar-track,\
+                    .a-scrollbar-fill-auto::-webkit-scrollbar-track {\
+                        background: transparent;\
+                    }\
+                    .a-scrollbar-fill::-webkit-scrollbar-thumb,\
+                    .a-scrollbar-fill-auto::-webkit-scrollbar-thumb {\
+                        border-radius: 5px !important;\
+                        -moz-background-clip: padding;\
+                        -webkit-background-clip: padding;\
+                        background-clip: padding-box;\
+                        border: 3px solid rgba( 255,255,255,0) !important;\
+                        background: #c1c1c2 !important;\
+                        background-clip: content-box !important;\
+                    }\
+                    .a-scrollbar-fill::-webkit-scrollbar-corner,\
+                    .a-scrollbar-fill-auto::-webkit-scrollbar-corner {\
+                        background: transparent !important;\
+                    }\
+                    .a-scrollbar-fill::-webkit-scrollbar-thumb:vertical:active,\
+                    .a-scrollbar-fill-auto::-webkit-scrollbar-thumb:vertical:active {\
+                        border-radius: 0 !important;\
+                        border-left-width: 0 !important;\
+                        border-right-width: 0 !important;\
+                    }\
+                    .a-scrollbar-fill::-webkit-scrollbar-thumb:horizontal:active,\
+                    .a-scrollbar-fill-auto::-webkit-scrollbar-thumb:horizontal:active {\
+                        border-radius: 0 !important;\
+                        border-top-width: 0 !important;\
+                        border-bottom-width: 0 !important;\
+                    }\
+    ';
+
+    var scrollbar_node = document.createElement('style');
+    scrollbar_node.innerHTML = scrollbar_style_str;
+    document.body.appendChild( scrollbar_node );
+
+    // needed the scrollbar editing before measuring....
+    var inner = document.createElement('p');
+    inner.style.width = "100%";
+    inner.style.height = "200px";
+
+    var outer = document.createElement('div');
+    outer.style.position = "absolute";
+    outer.style.top = "0px";
+    outer.style.left = "0px";
+    outer.style.visibility = "hidden";
+    outer.style.width = "200px";
+    outer.style.height = "150px";
+    outer.style.overflow = "hidden";
+    outer.className = "a-scrollbar-fill";
+    outer.appendChild (inner);
+
+    document.body.appendChild (outer);
+    var w1 = inner.offsetWidth;
+    outer.style.overflow = 'scroll';
+    var w2 = inner.offsetWidth;
+    if (w1 == w2) w2 = outer.clientWidth;
+
+    document.body.removeChild (outer);
+
+    var scrollbarWidth = (w1 - w2);
+
+    // these two don't customize scrollbars (ie11 & < are alright)
+    // TODO: make this more function based at some point...
+    var is_firefox = !!navigator.userAgent.match(/firefox/i);
+    var is_edge = !!navigator.userAgent.match(/edge/i);
+
+    var style_str = '.a-scrollbar-fill-auto,.a-scrollbar-fill { position: relative; };';//width: 100% !important;  }';
+
+    if ( scrollbarWidth > 0 ) {
+        style_str = '.a-scrollbar-fill > * { width: calc( 100% + ' + scrollbarWidth + 'px ) !important; }';
+        style_str += '.a-scrollbar-margin-right { margin-right: ' + scrollbarWidth + 'px !important; }';
+    }
+
+    if ( is_firefox || is_edge ) {
+        style_str = '.a-scrollbar-fill-auto > * { width: 100% !important; }';
+        style_str += '.a-scrollbar-fill-auto { overflow-y: auto !important; overflow-x: hidden !important; }';
+    }else{
+        style_str = '.a-scrollbar-fill-auto > * { width: calc( 100% + ' + scrollbarWidth + 'px ) !important; }';
+        style_str += '.a-scrollbar-fill-auto { overflow-y: scroll !important; overflow-x: hidden !important; }';
+    }
+
+    style_str += '.a-scrollbar-fill{ overflow-y: scroll !important; overflow-x: hidden !important; }';
+
+    var node = document.createElement('style');
+    node.innerHTML = style_str;
+    document.body.appendChild(node);
+}
+
+if (document.readyState != 'loading') {
+    createScrollbarFill();
+} else {
+    document.addEventListener('DOMContentLoaded', createScrollbarFill);
+}
